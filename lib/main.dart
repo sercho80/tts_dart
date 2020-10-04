c7565f090b797cf63240a3417ae170db41c36a34
sercho80 <46472925+sercho80@users.noreply.github.com>
2020-10-04 18:22:09 +0200

    App Lista (?)

---
+++ lib/main.dart
@@ -0,0 +1,43 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/rendering.dart';
+import 'package:flutter_tts/flutter_tts.dart';
+
+void main() => runApp(TheApp());
+
+class TheApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        body: MyApp(),
+      ),
+    );
+  }
+}
+
+class MyApp extends StatelessWidget {
+  final FlutterTts tts = FlutterTts();
+
+  @override
+  Widget build(BuildContext context) {
+    TextEditingController txt = TextEditingController();
+
+    Future _speak(String str) async {
+      await tts.setLanguage("es-ES");
+      await tts.setPitch(1);
+      await tts.speak(str);
+    }
+
+    return Container(
+        alignment: Alignment.center,
+        child: Column(mainAxisSize: MainAxisSize.min, children: <Widget>[
+          TextFormField(
+            controller: txt,
+          ),
+          RaisedButton(
+            child: Text("TTS"),
+            onPressed: () => _speak(txt.text),
+          ),
+        ]));
+  }
+}
